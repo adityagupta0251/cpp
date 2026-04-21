b4d73407fe5c83f8361abe6ff4543a7ca40135e6
Adi.K <166922118+adityagupta0251@users.noreply.github.com>
2026-04-21 20:14:59 +0530

    functions basic syntax & definition

--- chapter_5/functions.cpp
+++ chapter_5/functions.cpp
@@ -6,7 +6,39 @@
 #include <iostream>
 using namespace std;
 
+
+// No return type : doesn't returns anything
+// function definition
+void printHello() {
+  cout << "hello\n";
+  
+}
+
+
 int main() {
-  cout << "Hello world!!" << endl;
+  // cout << "Hello world!!" << endl;
+  // In programing we uses functions for use as a tool to call it when we need to repetedly do some works
+  // Functions are basically takes input or as parameter & throws output , & where we need to assign its return type.
+  // Basicallt type of Output
+
+
+
+  // let say : Here int is it's return type 
+  // int main() {
+  //
+  //
+  //   return 0; 
+  // }
+
+
+
+  // Calling/Invoking a function :-
+  printHello();
+
+
+  
+  // int a = 10;
+  // int b = -5;
+  // cout << "Hello\n";
   return 0;
 }
