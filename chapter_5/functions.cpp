a97acc7e8e93bcc61fb476c00019a128516e8ca8
Adi.K <166922118+adityagupta0251@users.noreply.github.com>
2026-04-22 11:45:18 +0530

    added functions parameter

--- chapter_5/functions.cpp
+++ chapter_5/functions.cpp
@@ -9,10 +9,30 @@ using namespace std;
 
 // No return type : doesn't returns anything
 // function definition
-void printHello() {
+//void printHello() {
+//  cout << "hello\n";
+//
+//}
+
+
+
+// Returning value from a function
+int printHello() {
   cout << "hello\n";
-  
+  return 3;
 }
+
+
+
+// Parameters in functions
+// We are creating a function which evaluates the sum of two integers
+int sum(int a , int b){
+  int s = a + b;
+  return s;
+
+}
+
+
 
 
 int main() {
@@ -23,22 +43,40 @@ int main() {
 
 
 
-  // let say : Here int is it's return type 
+  // let say : Here int is it's return type
   // int main() {
   //
   //
-  //   return 0; 
+  //   return 0;
   // }
 
 
 
   // Calling/Invoking a function :-
-  printHello();
+  // printHello();
 
 
-  
+
   // int a = 10;
   // int b = -5;
   // cout << "Hello\n";
+
+
+
+  // Now using those retuned value
+  int val = printHello();
+  cout << "val = " << val << endl;
+
+
+ // Or We can direectly return it with
+ // cout << printHello() << endl;
+ //
+ //
+
+
+ // Calling a function & giving it the require a & b as parameter to test
+ cout << sum(10, 7) << endl;
+
+
   return 0;
 }
